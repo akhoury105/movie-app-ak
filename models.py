899c6f994be6fe83d6e43d6ae7ca28d621ed3ae1
Aaron Khoury <akhoury105@gmail.com>
2020-11-21 14:05:23 -0500

    Sets up Database Classes

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, Date
 from flask_sqlalchemy import SQLAlchemy
 
 database_path = os.environ['DATABASE_URL']
@@ -11,9 +11,42 @@ db = SQLAlchemy()
 
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
     db.create_all()
 
 
+'''
+Movie Class
+Contains title and date released
 
+'''
+class Movie(db.Model):
+    __tablename__ = 'Movies'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    release = Column(Date)
+
+    def __init__(self, title, release):
+        self.title = title
+        self.release = release
+
+'''
+Actor Class
+Contains actor's name, birthdate, and gender
+
+'''
+class Actor(db.Model):
+    __tablename__ = 'Actors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    gender = Column(String)
+    birthdate = Column(Date)
+
+    def __init__(self, name, gener, birthdate):
+        self.name = name
+        self.gender = gender
+        self.birthdate = birthdate
