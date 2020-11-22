9a080e99ec9bdc3416a9692325bd95afdd55f66b
Aaron Khoury <akhoury105@gmail.com>
2020-11-22 12:02:16 -0500

    Adds error handling andpoints to app.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify
 from models import setup_db
+import json
 
 def create_app():
 
@@ -9,6 +10,34 @@ def create_app():
     return app
 
 app = create_app()
+
+
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 422,
+        "message": "unprocessable"
+    }), 422
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "resource not found"
+    }), 404
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "bad request"
+    })
+
 
 if __name__ == '__main__':
     app.run()
