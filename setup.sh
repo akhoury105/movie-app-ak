1faeed7b33e3f05e423121211d6b6f027548bbf8
Aaron Khoury <akhoury105@gmail.com>
2020-12-01 15:59:28 -0500

    Moves 3rd party config settings to bash file

--- setup.sh
+++ setup.sh
@@ -1,1 +1,4 @@
 export DATABASE_URL="postgres://postgres:postgres@localhost:5432/movie-app-ak"
+export AUTH0_DOMAIN="dev-con.us.auth0.com"
+export API_AUDIENCE="casting"
+export ALGORITHMS=["RS256"]
